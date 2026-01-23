fc9b76e3d422c585a89db2e687aa224284acc2e1
zhangsihai <huahua13925zdb@163.com>
2026-01-23 15:30:19 +0800

    modify website prefix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from "next";
 
 export const dynamic = "force-static";
 
-const BASE_URL = "https://ezparquet.com";
+const BASE_URL = "https://www.ezparquet.com";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const routes = [
